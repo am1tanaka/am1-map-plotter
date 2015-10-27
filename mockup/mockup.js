2ddcfeee79fcf78d5a786c7a447abfc9f31f49dd
YuTanaka <YRK00337@nifty.com>
2015-10-27 12:26:42 +0900

    マーカー表示/情報ポップアップ

---
+++ mockup/mockup.js
@@ -0,0 +1,39 @@
+/**
+ * 変数
+ */
+var map;
+var points = [];
+
+/**
+ * Leafletの表示
+ */
+function initLeaflet(lat, lng) {
+  // LeafletのOSM表示
+  map = L.map('map').setView([lat, lng], 17);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+}
+
+function setMarker(datas) {
+  var mk;
+  var pop;
+  for(var i=0 ; i<datas.length ; i++) {
+    mk = L.marker(
+      [datas[i].lat,datas[i].lng],
+      {
+        title: datas[i].title,
+        alt: datas[i].tags[0]
+      }
+    );
+    pop =
+    "<h1>"+datas[i].title+"</h1>"
+    +"<img width='240px' src='./datas/"+datas[i].photo+"' alt='"+datas[i].title+"' />"
+    +"<p>Tags:";
+    for (var j=0 ; j<datas[i].tags.length ; j++) {
+      if (j > 0) pop+=", ";
+      pop += datas[i].tags[j];
+    }
+    mk.bindPopup(pop);
+    points.push(mk);
+    mk.addTo(map);
+  }
+}
