67752f70a6f10a9b557f0ec695e7e1d2ad04f825
YuTanaka <YRK00337@nifty.com>
2015-10-27 12:08:58 +0900

    Leafletの表示/仮データの実装

---
+++ mockup/datas/datas.js
@@ -0,0 +1,88 @@
+var datas = [
+  {
+    lat: 35.631678,
+    lng: 139.441477,
+    title: 'カルガモ',
+    photo: 'kamo/IMG_4637.JPG',
+    tags: ['カモ']
+  },
+  {
+    lat: 35.631709,
+    lng: 139.441892,
+    title: 'マガモ',
+    photo: 'kamo/IMG_4721.JPG',
+    tags: ['カモ']
+  },
+  {
+    lat: 35.633517,
+    lng: 139.445673,
+    title: 'マルガモ',
+    photo: 'kamo/IMG_4725.JPG',
+    tags: ['カモ']
+  },
+  {
+    lat: 35.633416,
+    lng: 139.445240,
+    title: 'コガモ',
+    photo: 'kamo/IMG_4745.JPG',
+    tags: ['カモ']
+  },
+
+  {
+    lat: 35.631515,
+    lng: 139.441431,
+    title: 'ハクセキレイ♀',
+    photo: 'seki/IMG_4768.JPG',
+    tags: ['セキレイ']
+  },
+  {
+    lat: 35.632679,
+    lng: 139.443834,
+    title: 'ハクセキレイ♂',
+    photo: 'seki/IMG_4777.JPG',
+    tags: ['セキレイ']
+  },
+  {
+    lat: 35.631562,
+    lng: 139.441175,
+    title: 'キセキレイ',
+    photo: 'seki/IMG_4786.JPG',
+    tags: ['セキレイ']
+  },
+  {
+    lat: 35.631928,
+    lng: 139.441679,
+    title: 'ハクセキレイ♂',
+    photo: 'seki/IMG_5013.JPG',
+    tags: ['セキレイ']
+  },
+
+  {
+    lat: 35.632102,
+    lng: 139.442001,
+    title: 'エナガ',
+    photo: 'enaga/IMG_3144.JPG',
+    tags: ['エナガ']
+  },
+  {
+    lat: 35.632076,
+    lng: 139.441942,
+    title: 'エナガ',
+    photo: 'enaga/IMG_3162.JPG',
+    tags: ['エナガ']
+  },
+  {
+    lat: 35.632141,
+    lng: 139.442033,
+    title: 'エナガ',
+    photo: 'enaga/IMG_3236.JPG',
+    tags: ['エナガ']
+  },
+  {
+    lat: 35.631420,
+    lng: 139.440790,
+    title: 'エナガ',
+    photo: 'enaga/IMG_4828.JPG',
+    tags: ['エナガ']
+  },
+];
