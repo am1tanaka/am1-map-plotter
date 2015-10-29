dbedcdbe6e46f83a44d4e408b3377f5c19bc4903
YuTanaka <YRK00337@nifty.com>
2015-10-29 12:41:17 +0900

    仮データに要素を追加

--- mockup/datas/datas.js
+++ mockup/datas/datas.js
@@ -4,28 +4,44 @@ var datas = [
     lng: 139.441477,
     title: 'カルガモ',
     photo: 'kamo/IMG_4637.JPG',
-    tags: ['カモ']
+    tags: ['カモ'],
+    datetime: '2015/10/19 15:23:19',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.631709,
     lng: 139.441892,
     title: 'マガモ',
     photo: 'kamo/IMG_4721.JPG',
-    tags: ['カモ']
+    tags: ['カモ'],
+    datetime: '2015/10/25 14:27:07',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.633517,
     lng: 139.445673,
     title: 'マルガモ',
     photo: 'kamo/IMG_4725.JPG',
-    tags: ['カモ']
+    tags: ['カモ'],
+    datetime: '2015/10/25 14:27:25',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.633416,
     lng: 139.445240,
     title: 'コガモ',
     photo: 'kamo/IMG_4745.JPG',
-    tags: ['カモ']
+    tags: ['カモ'],
+    datetime: '2015/10/25 14:31:51',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
 
   {
@@ -33,28 +49,44 @@ var datas = [
     lng: 139.441431,
     title: 'ハクセキレイ♀',
     photo: 'seki/IMG_4768.JPG',
-    tags: ['セキレイ']
+    tags: ['セキレイ'],
+    datetime: '2015/10/25 14:42:06',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.632679,
     lng: 139.443834,
     title: 'ハクセキレイ♂',
     photo: 'seki/IMG_4777.JPG',
-    tags: ['セキレイ']
+    tags: ['セキレイ'],
+    datetime: '2015/10/25 14:42:19',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.631562,
     lng: 139.441175,
     title: 'キセキレイ',
     photo: 'seki/IMG_4786.JPG',
-    tags: ['セキレイ']
+    tags: ['セキレイ'],
+    datetime: '2015/10/25 14:43:09',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.631928,
     lng: 139.441679,
     title: 'ハクセキレイ♂',
     photo: 'seki/IMG_5013.JPG',
-    tags: ['セキレイ']
+    tags: ['セキレイ'],
+    datetime: '2015/10/26 14:11:33',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
 
   {
@@ -62,27 +94,43 @@ var datas = [
     lng: 139.442001,
     title: 'エナガ',
     photo: 'enaga/IMG_3144.JPG',
-    tags: ['エナガ']
+    tags: ['エナガ'],
+    datetime: '2015/10/10 11:31:21',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.632076,
     lng: 139.441942,
     title: 'エナガ',
     photo: 'enaga/IMG_3162.JPG',
-    tags: ['エナガ']
+    tags: ['エナガ'],
+    datetime: '2015/10/10 11:31:42',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.632141,
     lng: 139.442033,
     title: 'エナガ',
     photo: 'enaga/IMG_3236.JPG',
-    tags: ['エナガ']
+    tags: ['エナガ'],
+    datetime: '2015/10/10 11:36:57',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
   {
     lat: 35.631420,
     lng: 139.440790,
     title: 'エナガ',
     photo: 'enaga/IMG_4828.JPG',
-    tags: ['エナガ']
+    tags: ['エナガ'],
+    datetime: '2015/10/25 14:47:01',
+    comment: '',
+    access: 'public',
+    author: 'YuTanaka'
   },
 ];
